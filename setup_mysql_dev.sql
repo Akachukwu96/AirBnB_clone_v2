3fa8e6169078587f0cc5b6cf1de4917ad676b233
FearGod12 <onyenikechukwudi@gmail.com>
2023-03-20 17:27:42 +0000

    fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
---  prepares a MySQL server for the project
--- database = hbnb_dev_db. A new user hbnb_dev (in localhost)
+--  script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_shema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,7 @@
---  prepares a MySQL server for the project
--- database = hbnb_dev_db
--- A new user hbnb_test (in localhost)
---  password of hbnb_test should be set to hbnb_test_pwd
+-- setting up MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_shema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;

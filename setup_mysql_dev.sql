aabeb4b3cc1ccec4f8907868e7a46cabf3346787
FearGod12 <onyenikechukwudi@gmail.com>
2023-03-20 17:17:38 +0000

    task 2 & 3 fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,9 +2,7 @@
 -- database = hbnb_dev_db
 -- A new user hbnb_dev (in localhost)
 --  password of hbnb_dev should be set to hbnb_dev_pwd
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_shema.* TO 'hbnb_dev'@'localhost';

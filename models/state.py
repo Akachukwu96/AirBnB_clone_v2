e62359b6a07e76e45987e877eb029438aab5a9aa
FearGod12 <onyenikechukwudi@gmail.com>
2023-03-21 19:21:20 +0000

    task 6: added some new features

--- models/state.py
+++ models/state.py
@@ -3,15 +3,23 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import os
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade="all, delete", backref="state")
-
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', cascade="all, delete", backref="state")
+    
+    @property
     def cities(self):
         '''returns the list of City instances
         with state_id equals to the current State.id'''
-        return [city for city in self.cities if city.state_id == self.id]
+        from models import storage
+        list_of_cities = []
+        dict_cities = storage.all(City)
+        for city in dict_cities.values():
+            if city.state_id == self.id:
+                list_of_cities.append(city)
+        return list_of_cities

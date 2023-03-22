820de6b70e38123af5bebc2e68e0a21953ff02a2
Akachukwu <105462943+Akachukwu96@users.noreply.github.com>
2023-03-22 18:02:03 +0100

    task 8

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+""" This script defines 
+Place Module for HBNB project """
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Float, String, ForeignKey, Integer

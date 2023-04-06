e9f57fdda6e625e3ad08a0e813d22988d29a9c76
FearGod12 <onyenikechukwudi@gmail.com>
2023-04-06 22:53:39 +0000

    more fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,9 @@ from fabric.api import local, run, put, get, env
 from datetime import datetime
 import os
 
-env.hosts = ['ubuntu@3.86.13.6', 'ubuntu@35.175.64.13']
+env.hosts = ['3.86.13.6', '35.175.64.13']
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_pack():
@@ -35,13 +37,12 @@ def do_deploy(archive_path):
 
         run('sudo mkdir -p {}'.format(destination))
         run('sudo tar -xvzf /tmp/{} -C {}'.format(archive_name, destination))
+        run('sudo rm -r /tmp/{}'.format(archive_name))
         run('sudo mv {}/web_static/* {}'.format(destination, destination))
         run('sudo rm -rf {}/web_static'.format(destination))
 
-        run('sudo rm -r /tmp/{}'.format(archive_name))
-
         run('sudo rm /data/web_static/current')  # Delete link
         run('sudo ln -sf {} /data/web_static/current'.format(deatination))
-        return False
+        return True
     except Exception:
         return False

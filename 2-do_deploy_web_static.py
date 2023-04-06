0da688272117026ca4a1e1b2da93a91171bff4bc
FearGod12 <onyenikechukwudi@gmail.com>
2023-04-06 22:58:47 +0000

    task 2 more fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,18 +13,6 @@ env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 
 
-def do_pack():
-    '''generates a .tgz archive from the contents of the web_static folder'''
-
-    local("mkdir -p versions")   # create the directory if it doesnt exist
-    arch_name = "web_static_{}".format(datetime.now().strftime("%Y%m%d%H%M%S"))
-    result = local('tar -czvf versions/{}.tgz web_static'.format(arch_name))
-    if result.succeeded:
-        return "/versions/{}".format(arch_name)
-    else:
-        return None
-
-
 def do_deploy(archive_path):
     '''distributes an archive to your web servers
     '''
